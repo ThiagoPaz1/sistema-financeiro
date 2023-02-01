3f8f087daa16dbc738a67b225cd3d879f9a45234
Thiago da Paz <thiagopazjunior@gmail.com>
2023-02-01 09:32:31 -0300

    feat: Created page Home.

--- sistema_financeiro/src/App.tsx
+++ sistema_financeiro/src/App.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router'
 
 import { Login } from './pages/Login'
 import { Registe } from './pages/Registe'
+import { Home } from './pages/Home'
 
 function App() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Registe />} />
       </Routes>
---
+++ sistema_financeiro/src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <>
+      <h1>Tela de Home</h1>
+    </>
+  )
+}
