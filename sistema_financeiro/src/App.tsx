83f248ed938018ee8d2ea53099c6fcae1203c27e
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-02-17 00:01:45 -0300

    Fazendo a validação de cadastro

--- sistema_financeiro/src/App.tsx
+++ sistema_financeiro/src/App.tsx
@@ -1,8 +1,10 @@
 import { Routes, Route } from 'react-router'
-
 import { Login } from './pages/Login'
 import { Registe } from './pages/Registe'
 import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Registe />} />
+        <Route path="/*" element={ <NotFound /> } />
       </Routes>
+      <ToastContainer />
     </div>
   )
 }
--- sistema_financeiro/src/pages/Registe/index.tsx
+++ sistema_financeiro/src/pages/Registe/index.tsx
@@ -1,7 +1,51 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import validRegister from "../../utils/validUser"
+import { toast } from 'react-toastify';
+
 export function Registe() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [validUser, setValidUser ] = useState(false)
+  const nav = useNavigate()
+
+  const validUsuario = () => {
+    if (name === "" || email === "" || password === "") {
+      return toast.error("Todos os campos deve ser preenchidos")
+    }
+
+    if(!validRegister(email, name, password)){
+        setValidUser(true)
+        return toast.error("Preencha os campos de forma correta")
+      }
+
+      return nav('/')
+  }
+
+
   return (
     <>
       <h1>Tela de cadastro</h1>
+      <form action="">
+          <div>
+            <label htmlFor="name">Nome:</label>
+            <input type="text" id="name" value={ name } onChange={ (event) => setName(event.target.value)} />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="text" id="email" value={ email } onChange={ (event) => setEmail(event.target.value)} />
+          </div>
+
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input type="text" id="password" value={ password } onChange={ (event) => setPassword(event.target.value)} />
+          </div>
+          { validUser && <p>Digite um e-mail valido <br /> Nome precisa ter ao menos 3 carácteres <br />  O password precisa ter no mínimo 6 caracteres, contendo letras e números </p> }
+        <button type="button" onClick={ validUsuario } >Cadastrar</button>
+
+      </form>
     </>
   )
 }
